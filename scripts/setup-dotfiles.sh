240497166a7156b592f8046355af5c2b4d899978
Luukas <luukasniemin@gmail.com>
2024-07-22 01:39:54 +0300

    Started making the setup-dotfiles script

--- scripts/setup-dotfiles.sh
+++ scripts/setup-dotfiles.sh
@@ -1,2 +1,12 @@
 # Write later
 # A script which setups the dotfiles (NOTE: you need to run the install-dependencies script first)
+
+check_bin() {
+    which $1 >> /dev/null
+    if [[ $? -eq 1 ]]; then
+        echo "Binary $1 is required to setup these dotfiles, exiting..."
+        exit 1
+    fi
+}
+
+check_bin "stow"
