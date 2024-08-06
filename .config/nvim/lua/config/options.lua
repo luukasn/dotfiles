fd494d9816fefeaa730891f6c76f8fda2b0f361b
Luukas <luukasniemin@gmail.com>
2024-08-07 00:46:53 +0300

    Added onedark colorsheme to alacritty and neovim

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,7 +1,7 @@
 vim.cmd("set relativenumber")
-vim.cmd("color gruvbox-material")
+vim.cmd("color onedark")
 vim.opt.expandtab = true -- Pressing TAB will insert spaces instead of TAB.
-vim.opt.tabstop = 4 -- A TAB character which is 4 spaces
-vim.opt.softtabstop = 4 -- Number of spaces inserted when pressing TAB
-vim.opt.shiftwidth = 4 -- Number of spaces inserted when indenting a character
+vim.opt.tabstop = 4      -- A TAB character which is 4 spaces
+vim.opt.softtabstop = 4  -- Number of spaces inserted when pressing TAB
+vim.opt.shiftwidth = 4   -- Number of spaces inserted when indenting a character
 vim.opt.conceallevel = 2
