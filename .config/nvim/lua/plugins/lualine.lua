1eab3d2f0d47d8a98729915d01474a53a5acf4c9
= <=>
2024-08-16 00:59:19 +0300

    Changed neovim lualine configuration

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -1,13 +1,15 @@
 -- This plugin gives you a pretty bottom line to your editor
 
 return {
-	"nvim-lualine/lualine.nvim",
-	dependencies = { "nvim-tree/nvim-web-devicons" },
-	config = function()
-		require("lualine").setup({
-			options = {
-				icons_enabled = true,
-			},
-		})
-	end,
+    "nvim-lualine/lualine.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        require("lualine").setup({
+            options = {
+                icons_enabled = true,
+                section_separators = "",
+                component_separators = "|"
+            },
+        })
+    end,
 }
