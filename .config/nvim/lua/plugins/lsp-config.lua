1db0f49978552d4825d352507e36de9f2b8f0f1e
= <=>
2024-08-20 23:50:06 +0300

    Added marksman lsp to mason ensure_intalled

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -1,52 +1,55 @@
 return {
-	{
-		"williamboman/mason.nvim",
-		lazy = false,
-		config = function()
-			require("mason").setup()
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		lazy = false,
-		config = function()
-			require("mason-lspconfig").setup({
-				ensure_installed = { "lua_ls", "clangd", "tsserver" },
-			})
-		end,
-	},
-	{
-		"neovim/nvim-lspconfig",
-		lazy = false,
-		config = function()
-			-- CMP nvim is the autocompletion engine.
-			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = function()
+            require("mason").setup()
+        end,
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        lazy = false,
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "lua_ls", "clangd", "tsserver", "marksman" },
+            })
+        end,
+    },
+    {
+        "neovim/nvim-lspconfig",
+        lazy = false,
+        config = function()
+            -- CMP nvim is the autocompletion engine.
+            local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
-			local lspconfig = require("lspconfig")
-			lspconfig.lua_ls.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.clangd.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.tsserver.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.marksman.setup({
-				capabilities = capabilities,
-			})
-			lspconfig.svelte.setup({
-				capabilities = capabilities,
-			})
+            local lspconfig = require("lspconfig")
+            lspconfig.lua_ls.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.clangd.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.tsserver.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.marksman.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.svelte.setup({
+                capabilities = capabilities,
+            })
+            lspconfig.tailwindcss.setup({
+                capabilities = capabilities
+            })
 
-			-- se: Show Explanation
-			vim.keymap.set("n", "<leader>se", vim.diagnostic.open_float, {})
-			-- sd: Show definition
-			vim.keymap.set("n", "<leader>sd", vim.lsp.buf.hover, {})
-			-- gd: Go to definition
-			vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
-			-- ca: Code Action
-			vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
-		end,
-	},
+            -- se: Show Explanation
+            vim.keymap.set("n", "<leader>se", vim.diagnostic.open_float, {})
+            -- sd: Show definition
+            vim.keymap.set("n", "<leader>sd", vim.lsp.buf.hover, {})
+            -- gd: Go to definition
+            vim.keymap.set("n", "<leader>gd", vim.lsp.buf.definition, {})
+            -- ca: Code Action
+            vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
+        end,
+    },
 }
