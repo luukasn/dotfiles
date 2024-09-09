168f59a9cf507524fe3ff93873f7f45ce666a489
Luukas <luukasniemin@gmail.com>
2024-09-10 00:34:37 +0300

    Neovim and fastfetch configuration

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -4,4 +4,13 @@ vim.opt.expandtab = true -- Pressing TAB will insert spaces instead of TAB.
 vim.opt.tabstop = 4      -- A TAB character which is 4 spaces
 vim.opt.softtabstop = 4  -- Number of spaces inserted when pressing TAB
 vim.opt.shiftwidth = 4   -- Number of spaces inserted when indenting a character
-vim.opt.conceallevel = 2
+
+-- Function to set the conceallevel to 2 if we're in a markdown file
+vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function()
+        -- vim.print(vim.bo.filetype)
+        if vim.bo.filetype == "markdown" then
+            vim.opt.conceallevel = 2
+        end
+    end
+})
