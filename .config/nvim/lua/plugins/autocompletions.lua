bbd3bd3c50ebd9fbbd3b1ce338e8e2e6f16d5157
= <=>
2024-09-24 14:30:17 +0300

    Added new dotfiles

--- .config/nvim/lua/plugins/autocompletions.lua
+++ .config/nvim/lua/plugins/autocompletions.lua
@@ -1,118 +1,134 @@
 return {
-	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
-		"hrsh7th/cmp-buffer",
-	},
-	{
-		"hrsh7th/cmp-path",
-	},
-	{
-		"L3MON4D3/LuaSnip",
-		dependencies = {
-			"saadparwaiz1/cmp_luasnip",
-			"rafamadriz/friendly-snippets",
-		},
-	},
-	{
-		"onsails/lspkind.nvim", -- VS-code like UI for the cmp autosuggestion engine
-		lazy = false,
-	},
-	{
-		"hrsh7th/nvim-cmp",
-		config = function()
-			local cmp = require("cmp")
-			require("luasnip.loaders.from_vscode").lazy_load()
-			local luasnip = require("luasnip")
-			require("config.snippets")
+    {
+        "hrsh7th/cmp-nvim-lsp", -- Get autocompletion from the lsp
+    },
+    -- {
+    --     "hrsh7th/cmp-buffer", -- Get autocompletion from the current buffer texts
+    -- },
+    {
+        "hrsh7th/cmp-path", -- Get path autocompletions
+    },
+    {
+        "hrsh7th/cmp-cmdline", -- Get commandline autocompletions
+    },
+    {
+        "L3MON4D3/LuaSnip",
+        dependencies = {
+            "saadparwaiz1/cmp_luasnip",
+            "rafamadriz/friendly-snippets",
+        },
+    },
+    {
+        "onsails/lspkind.nvim", -- VS-code like UI for the cmp autosuggestion engine
+        lazy = false,
+    },
+    {
+        "hrsh7th/nvim-cmp",
+        config = function()
+            local cmp = require("cmp")
+            require("luasnip.loaders.from_vscode").lazy_load()
+            local luasnip = require("luasnip")
+            require("config.snippets")
 
-			local has_words_before = function()
-				unpack = unpack or table.unpack
-				local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-				return col ~= 0
-					and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-			end
+            local has_words_before = function()
+                unpack = unpack or table.unpack
+                local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+                return col ~= 0
+                    and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+            end
 
-			cmp.setup({
-				snippet = {
-					-- REQUIRED - you must specify a snippet engine
-					expand = function(args)
-						-- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-						require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
-						-- require('snippy').expand_snippet(args.body) -- For `snippy` users.
-						-- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
-					end,
-				},
-				window = {
-					completion = cmp.config.window.bordered({ border = "shadow" }),
-					documentation = cmp.config.window.bordered({ border = "shadow" }),
-				},
-				mapping = cmp.mapping.preset.insert({
-					["<C-k>"] = cmp.mapping.scroll_docs(-4),
-					["<C-j>"] = cmp.mapping.scroll_docs(4),
-					["<C-Space>"] = cmp.mapping.complete(),
-					-- ['<C-e>'] = cmp.mapping.abort(),
-					["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-					["<Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_next_item()
-						elseif has_words_before() then
-							cmp.complete()
-						else
-							fallback()
-						end
-					end),
+            cmp.setup({
+                snippet = {
+                    -- REQUIRED - you must specify a snippet engine
+                    expand = function(args)
+                        -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
+                        require("luasnip").lsp_expand(args.body) -- For `luasnip` users.
+                        -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
+                        -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
+                    end,
+                },
+                window = {
+                    completion = cmp.config.window.bordered({ border = "shadow" }),
+                    documentation = cmp.config.window.bordered({ border = "shadow" }),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-k>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-j>"] = cmp.mapping.scroll_docs(4),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    -- ['<C-e>'] = cmp.mapping.abort(),
+                    ["<CR>"] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_next_item()
+                        elseif has_words_before() then
+                            cmp.complete()
+                        else
+                            fallback()
+                        end
+                    end),
 
-					["<S-Tab>"] = cmp.mapping(function(fallback)
-						if cmp.visible() then
-							cmp.select_prev_item()
-						elseif luasnip.jumpable(-1) then
-							luasnip.jump(-1)
-						else
-							fallback()
-						end
-					end, { "i", "s" }),
-				}),
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        if cmp.visible() then
+                            cmp.select_prev_item()
+                        elseif luasnip.jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                }),
 
-				["<C-e>"] = cmp.mapping(function(fallback)
-					vim.print("Hello, from a function!")
-					if luasnip.expand_or_jumpable() then
-						print("Context is jumpable")
-						luasnip.expand_or_jump()
-					else
-						print("Could not jump to the next snippet")
-						fallback()
-					end
-				end),
-				-- Configure which sources the completion comes from
-				sources = cmp.config.sources({
-					{ name = "nvim_lsp" }, -- Accept sources from the nvip lsp client.
-					{ name = "luasnip" }, -- For luasnip users.
-					{ name = "buffer" },
-					{ name = "path" },
-				}, {
-					{
-						name = "buffer",
-						options = {
-							keyword_length = 4,
-						},
-					},
-				}),
-				experimental = {
-					ghost_text = true,
-				},
-				formatting = {
-					format = require("lspkind").cmp_format({
-						mode = "symbol",
-						maxwidth = 50,
+                ["<C-e>"] = cmp.mapping(function(fallback)
+                    vim.print("Hello, from a function!")
+                    if luasnip.expand_or_jumpable() then
+                        print("Context is jumpable")
+                        luasnip.expand_or_jump()
+                    else
+                        print("Could not jump to the next snippet")
+                        fallback()
+                    end
+                end),
+                -- Configure which sources the completion comes from
+                sources = cmp.config.sources({
+                    { name = "nvim_lsp" }, -- Accept sources from the nvip lsp client.
+                    { name = "luasnip" },  -- For luasnip users.
+                    { name = "buffer" },
+                    { name = "path" },
+                }, {
+                    {
+                        name = "buffer",
+                        options = {
+                            keyword_length = 4,
+                        },
+                    },
+                }),
+                experimental = {
+                    ghost_text = true,
+                },
+                formatting = {
+                    format = require("lspkind").cmp_format({
+                        mode = "symbol",
+                        maxwidth = 50,
 
-						before = function(entry, vim_item)
-							return vim_item
-						end,
-					}),
-				},
-			})
-		end,
-	},
+                        before = function(entry, vim_item)
+                            return vim_item
+                        end,
+                    }),
+                },
+            })
+
+            cmp.setup.cmdline(":", {
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = "path" },
+                    {
+                        name = "cmdline",
+                        option = {
+                            ignore_cmds = { "!" }
+                        }
+                    }
+                })
+            })
+        end,
+    },
 }
