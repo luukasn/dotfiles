e8e5014d70f04164875cc762d5e29d2ad29aa541
Luukas <luukasniemin@gmail.com>
2024-09-26 01:21:45 +0300

    nvim: added gitsigns plugin

---
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -0,0 +1,12 @@
+return {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+        require("gitsigns").setup {
+            current_line_blame_opts = {
+                delay = 500
+            },
+        }
+
+        vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+    end
+}
