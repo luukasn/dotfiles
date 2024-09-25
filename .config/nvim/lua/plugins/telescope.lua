d5e0699d41de05bb1970130f561f9af730aac60e
Luukas <luukasniemin@gmail.com>
2024-09-26 01:22:14 +0300

    nvim: removed telescope git branch keaymap

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,31 +1,30 @@
 -- This is a plugin which provides us with fuzzy finding, file finder etc etc...
 
 return {
-	{
-		"nvim-telescope/telescope.nvim",
-		tag = "0.1.5",
-		dependencies = { "nvim-lua/plenary.nvim" }, -- This is a dependency which just provides API functions for the lua runtime in neovim
-		config = function()
-			local builtin = require("telescope.builtin")
-			vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
-			vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
-			vim.keymap.set("n", "<leader>man", builtin.man_pages, {})
-			vim.keymap.set("n", "<leader>gb", builtin.git_branches, {})
-			vim.keymap.set("n", "<leader>gc", builtin.git_commits, {})
-		end,
-	},
-	{
-		"nvim-telescope/telescope-ui-select.nvim",
-		config = function()
-			require("telescope").setup({
-				extensions = {
-					["ui-select"] = {
-						require("telescope.themes").get_dropdown({}),
-					},
-				},
-			})
+    {
+        "nvim-telescope/telescope.nvim",
+        tag = "0.1.5",
+        dependencies = { "nvim-lua/plenary.nvim" }, -- This is a dependency which just provides API functions for the lua runtime in neovim
+        config = function()
+            local builtin = require("telescope.builtin")
+            vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+            vim.keymap.set("n", "<leader>lg", builtin.live_grep, {})
+            vim.keymap.set("n", "<leader>man", builtin.man_pages, {})
+            vim.keymap.set("n", "<leader>gc", builtin.git_commits, {})
+        end,
+    },
+    {
+        "nvim-telescope/telescope-ui-select.nvim",
+        config = function()
+            require("telescope").setup({
+                extensions = {
+                    ["ui-select"] = {
+                        require("telescope.themes").get_dropdown({}),
+                    },
+                },
+            })
 
-			require("telescope").load_extension("ui-select")
-		end,
-	},
+            require("telescope").load_extension("ui-select")
+        end,
+    },
 }
