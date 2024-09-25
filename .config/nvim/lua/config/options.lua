4fc886d61e2c063b50f5d5d0a494255ced81d170
Luukas <luukasniemin@gmail.com>
2024-09-26 01:28:20 +0300

    nvim: disabled vim diagnostic signs
    Disabled vim diagnostic signs as they would not let gitsings be shown on
    a line where a diagnostic sign was on.

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -6,6 +6,10 @@ vim.opt.tabstop = 4      -- A TAB character which is 4 spaces
 vim.opt.softtabstop = 4  -- Number of spaces inserted when pressing TAB
 vim.opt.shiftwidth = 4   -- Number of spaces inserted when indenting a character
 
+vim.diagnostic.config({
+    signs = false
+})
+
 -- Function to set the conceallevel to 2 if we're in a markdown file
 vim.api.nvim_create_autocmd("BufEnter", {
     callback = function()
