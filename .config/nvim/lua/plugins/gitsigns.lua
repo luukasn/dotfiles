f0a114c627ea249f881911c3043e4e16d9a28f42
Luukas <luukasniemin@gmail.com>
2024-09-26 01:39:21 +0300

    nvim: added gitsigns keymapping

--- .config/nvim/lua/plugins/gitsigns.lua
+++ .config/nvim/lua/plugins/gitsigns.lua
@@ -5,8 +5,10 @@ return {
             current_line_blame_opts = {
                 delay = 500
             },
+            current_line_blame_formatter = ' <author>, <author_time:%R>: "<summary>"',
         }
 
         vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+        vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
     end
 }
