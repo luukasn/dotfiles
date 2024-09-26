b1eed74dac82de8dc9721e817286d251ba964f2e
Luukas <luukasniemin@gmail.com>
2024-09-26 13:22:53 +0300

    nvim: added custom prompt to gp.nvim

--- .config/nvim/lua/plugins/gp.lua
+++ .config/nvim/lua/plugins/gp.lua
@@ -1,6 +1,15 @@
 return {
     "robitx/gp.nvim",
     config = function()
+        local custom_prompts = {
+            code_assistant = "You are an AI assistant for a programmer\n\n"
+                .. "Your main purpose is to help with programming topics and coding, follow these guides:\n\n"
+                .. "- Always format code blocks using markdown syntax for code blocks\n"
+                .. "- When the user is asking for code generation, briefly explain what the code does\n"
+                .. "- When generating code for the user, add brief comments to the code explaining the code\n"
+                .. "- If you need more clarification on some parts, ask the user.\n"
+        }
+
         local conf = {
             -- For customization, refer to Install > Configuration in the Documentation/Readme
             openai_api_key = os.getenv("OPENAI_API_KEY"),
@@ -13,7 +22,7 @@ return {
                     chat = true,
                     command = true,
                     model = { model = "gpt-4" },
-                    system_prompt = require("gp.defaults").chat_system_prompt,
+                    system_prompt = custom_prompts.code_assistant,
                 }
             },
         }
