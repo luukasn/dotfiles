c6182329b5ae4d49f85e3e872c76b95a45d8d771
= <=>
2024-10-03 00:11:01 +0300

    nvim: python to ensure_installed lsps

--- .config/nvim/lua/plugins/lsp-config.lua
+++ .config/nvim/lua/plugins/lsp-config.lua
@@ -11,7 +11,7 @@ return {
         lazy = false,
         config = function()
             require("mason-lspconfig").setup({
-                ensure_installed = { "lua_ls", "clangd", "ts_ls", "marksman" },
+                ensure_installed = { "lua_ls", "clangd", "ts_ls", "marksman", "pyright" },
             })
         end,
     },
