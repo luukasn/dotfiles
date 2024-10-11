f74b0cec5ebf711d525b4ee599b40b3d7a9b7813
Luukas <luukasniemin@gmail.com>
2024-10-11 17:26:39 +0300

    nvim: attached_lsp() returns all attached clients

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,14 +4,32 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
+        -- Function to return all the attached lsps
+        -- in a string
         local function attached_lsp()
-            local lsp = vim.lsp.buf_get_clients()[1]
+            local attached_lsps = vim.lsp.buf_get_clients()
 
-            if not (lsp == nil) and (lsp.name) then
-                return [[a ]] .. lsp.name
-            else
-                return [[a none]]
+            local lsp_string = [[]]
+
+            -- Check if the list is empty, if it is then no
+            -- lsp servers are attached to the buffer.
+            if attached_lsps[1] == nil then
+                return lsp_string .. "none"
             end
+
+            -- Loop over the attached clients and
+            -- add the names to the lsp_string
+            for i, value in ipairs(attached_lsps) do
+                if value.name ~= nil then
+                    if i == 1 then
+                        lsp_string = lsp_string .. value.name
+                    else
+                        lsp_string = lsp_string .. ", " .. value.name
+                    end
+                end
+            end
+
+            return lsp_string
         end
 
         require("lualine").setup({
@@ -21,8 +39,8 @@ return {
                 component_separators = "|",
             },
             sections = {
-                lualine_a = { { 'mode', fmt = function(str) return " " .. str end } },
-                lualine_x = { attached_lsp, 'filetype' }
+                lualine_a = { { 'mode', icon = { '' } } },
+                lualine_x = { { attached_lsp, icon = { '󰒍', color = { fg = '#64B5F6' } } }, 'filetype' }
             }
         })
     end,
