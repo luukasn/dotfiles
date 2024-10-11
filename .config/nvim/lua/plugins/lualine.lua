28639932d7488a1c16e0f64f7d2c7368bfb5a5ca
Luukas <luukasniemin@gmail.com>
2024-10-11 15:42:45 +0300

    nvim: lualine show attached lsp

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -4,12 +4,26 @@ return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
+        local function attached_lsp()
+            local lsp = vim.lsp.buf_get_clients()[1]
+
+            if not (lsp == nil) and (lsp.name) then
+                return [[a ]] .. lsp.name
+            else
+                return [[a none]]
+            end
+        end
+
         require("lualine").setup({
             options = {
                 icons_enabled = true,
                 section_separators = "",
-                component_separators = "|"
+                component_separators = "|",
             },
+            sections = {
+                lualine_a = { { 'mode', fmt = function(str) return " " .. str end } },
+                lualine_x = { attached_lsp, 'filetype' }
+            }
         })
     end,
 }
