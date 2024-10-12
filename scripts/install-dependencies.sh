a0ac9cc8dd8ba2f25e80a3f0f098b5b256bb662a
Luukas <luukasniemin@gmail.com>
2024-10-12 03:20:31 +0300

    script: add starship prompt as a dependency

--- scripts/install-dependencies.sh
+++ scripts/install-dependencies.sh
@@ -8,7 +8,7 @@ source ./lib/utils.sh
 arch_linux_full_install_packages=(
     "base-devel" "devtools" "stow" "nodejs" "npm" "fzf"
     "sway" "waybar" "swaybg" "slurp" "grim" "wl-clipboard" 
-    "brightnessctl" "ttf-jetbrains-mono-nerd" "neovim"
+    "brightnessctl" "ttf-jetbrains-mono-nerd" "neovim" "starship"
 )
 
 if [[ "$XDG_SESSION_TYPE" == "wayland" ]]; then
